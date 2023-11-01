71ba6808c6568c35f129b909c041f2aa6fbe889b
Choonho Son <choonhoson@megazone.com>
2023-11-01 11:13:45 +0900

    Update a1.py
    Signed-off-by: Choonho Son <choonhoson@megazone.com>

--- src/goldminer/automations/a1.py
+++ src/goldminer/automations/a1.py
@@ -6,15 +6,17 @@ from pytz import timezone
 from goldminer.tasks.naver import search_news
 
 today = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %H")
+search_key = "국내증시"
 
-result = search_news("국내증시", 5)
+result = search_news(search_key, 5)
 content = ""
 for item in result:
     content = content + "<h2>" + item['title'] + "</h2>" + "<br>" + item['description'] + "<br>" + \
             "원문: <a href=\"" + item['link'] + "\" target=\"_blank\">" + item['link'] + "</a>" + "<br><br>"
 
+title = f"{search_key} {today}시 주요 뉴스"
 from goldminer.tasks.tistory import write_tistory_post
-write_tistory_post("국내증시 " + today, content)
+write_tistory_post(title, content)
 
 #from goldminer.tasks.blogger import create_blogger_post
-#create_blogger_post("국내 증시 " + today, content)
+#create_blogger_post(title, content)
